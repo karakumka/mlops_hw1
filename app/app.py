5450298777ec151460073ae8f926b086190d0a9d
Наталья Тимшина <natalia.timshina@gmail.com>
2026-05-23 03:08:11 +0300

    main files added

---
+++ app/app.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import pandas as pd
+import time
+import logging
+from watchdog.observers.polling import PollingObserver as Observer
+from watchdog.events import FileSystemEventHandler
+from datetime import datetime
+
+sys.path.append(os.path.abspath('./src'))
+from preprocessing import preprocess_transform
+from scorer import model_xgb, onehot_encoder, catboost_encoder, feature_columns, make_prediction, json_features, plot_density
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('/app/logs/service.log'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
+
+
+class ProcessingService:
+    def __init__(self):
+        logger.info('Initializing ProcessingService...')
+        self.input_dir = '/app/input'
+        self.output_dir = '/app/output'
+        logger.info('Service initialized')
+
+    def process_single_file(self, file_path):
+        try:
+            logger.info('Processing file: %s', file_path)
+            input_df = pd.read_csv(file_path)
+
+            logger.info('Starting preprocessing')
+            processed_df = preprocess_transform(input_df, onehot_encoder, catboost_encoder, feature_columns)
+            
+            logger.info('Making prediction')
+            submission = make_prediction(processed_df, file_path)
+            
+            logger.info('Preparing submission file')
+
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            base_filename = os.path.basename(file_path)
+
+            output_filename = f"predictions_{timestamp}_{base_filename}"
+            submission.to_csv(os.path.join(self.output_dir, output_filename), index=False)
+            logger.info("Predictions saved to: %s", output_filename)
+
+            logger.info("Generating density plot")
+            y_pred = model_xgb.predict_proba(processed_df)[:, 1]
+            img_filename = f"preds_density_{timestamp}_{base_filename}.png"
+            plot_density(y_pred, os.path.join(self.output_dir, img_filename))
+
+            logger.info("Collecting info on top features")
+            json_filename = f"top5_features_{timestamp}_{base_filename}.json"
+            json_features(model_xgb, feature_columns, os.path.join(self.output_dir, json_filename))
+
+            logger.info('The process is completed')
+
+        except Exception as e:
+            logger.error('Error processing file %s: %s', file_path, e, exc_info=True)
+            return
+
+
+class FileHandler(FileSystemEventHandler):
+    def __init__(self, service):
+        self.service = service
+
+    def on_created(self, event):
+        if not event.is_directory and event.src_path.endswith(".csv"):
+            logger.debug('New file detected: %s', event.src_path)
+            self.service.process_single_file(event.src_path)
+
+if __name__ == "__main__":
+    logger.info('Starting ML scoring service...')
+    service = ProcessingService()
+    observer = Observer(timeout=2)
+    observer.schedule(FileHandler(service), path=service.input_dir, recursive=False)
+    observer.start()
+    logger.info('File observer started')
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info('Service stopped by user')
+        observer.stop()
+    observer.join()
---
+++ src/scorer.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import logging
+import joblib
+import json
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+
+logger.info('Importing pretrained entities...')
+
+# Import pretrained entities
+model_xgb = joblib.load("trained_entities/model.pkl")
+onehot_encoder = joblib.load("trained_entities/onehot_encoder.pkl")
+catboost_encoder = joblib.load("trained_entities/catboost_encoder.pkl")
+feature_columns = joblib.load("trained_entities/feature_columns.pkl")
+
+# Define optimal threshold
+model_th = 0.97
+logger.info('Pretrained entities imported successfully...')
+
+# Make prediction
+def make_prediction(dt, path_to_file):
+
+    # Make submission dataframe
+    submission = pd.DataFrame({
+        'index':  pd.read_csv(path_to_file).index,
+        'prediction': (model_xgb.predict_proba(dt)[:, 1] > model_th) * 1
+    })
+    logger.info('Prediction complete for file: %s', path_to_file)
+
+    # Return proba for positive class
+    return submission
+
+# Save information on top features
+def json_features(model, feature_columns, path_to_file):
+    
+    feat_importances = pd.Series(model.feature_importances_, index=feature_columns)
+    top5 = {str(k): float(v) for k, v in feat_importances.nlargest(5).to_dict().items()}
+
+    with open(path_to_file, "w", encoding="utf-8") as json_file:
+        json.dump(top5, json_file, indent=4)
+
+    logger.info(f"Файл с топ-5 признаками сохранен: {path_to_file}")
+
+    return path_to_file
+
+# Plot predicted densities
+def plot_density(y_pred, path_to_file):
+
+    fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+    
+    sns.histplot(y_pred, kde=True, stat="density", color="darkcyan", bins=50, alpha=0.6, ax=ax[0])
+    ax[0].axvline(x=0.97, color="indigo", linestyle="--", linewidth=1.5, label="best threshold")
+    ax[0].set_title("Распределение предсказанных вероятностей")
+    ax[0].set_xlabel("Probability")
+    ax[0].set_ylabel("Density")
+    ax[0].set_xlim(0, 1)
+    ax[0].grid(False)
+    ax[0].legend()
+    
+    sns.kdeplot(y_pred, bw_adjust=0.5, color="teal", linewidth=2, fill=True, alpha=0.1, ax=ax[1])
+    ax[1].set_yscale("log")
+    ax[1].axvline(x=0.97, color="indigo", linestyle="--", linewidth=1.5, label="best threshold")
+    ax[1].set_title("Распределение предсказанных вероятностей \n(log scale Y)")
+    ax[1].set_xlabel("Probability")
+    ax[1].set_ylabel("Log density")
+    ax[1].set_xlim(0, 1)
+    ax[1].grid(False)
+    ax[1].legend()
+    
+    plt.savefig(path_to_file, dpi=300)
+    plt.close()
+
+    logger.info(f"Файл с распределением предсказанных вероятностей сохранен: {path_to_file}")
+
+    return path_to_file
